0e81c4a272c0f7d8db7826b673b6c9c81f0216dd
Aymeric Buriez <aymeric.buriez@hotmail.fr>
2024-12-27 22:31:55 +0100

    format to create ros package

---
+++ launch/pendulum.launch.py
@@ -0,0 +1,32 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.actions import ExecuteProcess
+import os
+
+def generate_launch_description():
+    # Récupération du chemin absolu de ce fichier
+    package_dir = os.path.dirname(__file__)
+    
+    # Chemin relatif pour le modèle SDF
+    model_path = os.path.join(package_dir, '../models/double_pendulum_rail.sdf')
+    
+    # Chemin relatif pour le fichier YAML de configuration
+    bridge_config_path = os.path.join(package_dir, '../config/bridge_config.yaml')
+    
+    # Gazebo server process
+    gazebo_server = ExecuteProcess(
+        cmd=['gz', 'sim', model_path],
+        output='screen'
+    )
+
+    # ROS-Gazebo bridge utilisant le fichier YAML
+    ros_gz_bridge = Node(
+        package='ros_gz_bridge',
+        executable='parameter_bridge',
+        parameters=[bridge_config_path]
+    )
+    
+    return LaunchDescription([
+        gazebo_server,
+        ros_gz_bridge,
+    ])
