0eab920d1464d01bb53111e2da45c925afda7bc5
Aymeric Buriez <aymeric.buriez@hotmail.fr>
2024-12-28 21:06:55 +0100

    Link ros-> gazebo works : update bridge configuration and speed publisher

--- src/projetIA/projetIA/speed_publisher.py
+++ src/projetIA/projetIA/speed_publisher.py
@@ -3,7 +3,7 @@
 
 """
 To change the speed during the simulation, call the set_speed service:
-ros2 service call /set_speed std_srvs/srv/SetBool "{data: 1.5}"
+ros2 service call /set_speed std_srvs/srv/SetFloat "{data: 1.5}"
 """
 import rclpy
 from rclpy.node import Node
@@ -15,8 +15,19 @@ class SpeedPublisher(Node):
         super().__init__('speed_publisher')
         self.publisher_ = self.create_publisher(Float64, '/trolley_speed_cmd', 10)
         self.timer = self.create_timer(0.1, self.timer_callback)
-        self.speed = 0.0  # Initial speed value
-        self.srv = self.create_service(SetBool, 'set_speed', self.set_speed_callback)
+        self.speed = 1.0  # Initial speed value
+        # self.srv = self.create_service(SetFloat, 'set_speed', self.set_speed_callback)  # Create the service
+
+    # def set_speed_callback(self, request, response):
+    #     """
+    #     Callback function to handle speed change requests.
+
+    #     This function is called when a request is made to the set_speed service.
+    #     """
+    #     self.speed = request.data
+    #     response.success = True
+    #     response.message = f"Speed set to {self.speed}"
+    #     return response
 
     def timer_callback(self):
         """
@@ -27,12 +38,6 @@ class SpeedPublisher(Node):
         msg = Float64()
         msg.data = self.speed
         self.publisher_.publish(msg)
-
-    def set_speed_callback(self, request, response):
-        self.speed = request.data
-        response.success = True
-        response.message = f'Speed set to {self.speed}'
-        return response
 
 def main(args=None):
     rclpy.init(args=args)
